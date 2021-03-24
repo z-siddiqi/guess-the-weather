99f687522d665e40342a7ff9c2a86108c37e7870
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-03-24 09:50:26 +0000

    refactor getWeather func

--- public/script.js
+++ public/script.js
@@ -2,21 +2,27 @@ let form = document.getElementById("userGuessForm");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     let city = document.getElementById("city").value;
-    let weatherCondition = document.querySelectorAll("input[name='condition']")[0].value;
-    let temperature = document.querySelectorAll("input[name='temperature']")[0].value;
-    getWeather(city);
+    getWeather(city)
+        .then(json => {
+            let userGuess = {
+                weatherCondition: document.querySelector("input[name='condition']:checked").value,
+                temperature: +document.querySelector("input[name='temperature']").value,
+            };
+            let answer = {
+                weatherCondition: json.weather[0].main,
+                temperature: Math.round(parseFloat(json.main.temp) - 273.15),  // convert to celsius
+            };
+            console.log(userGuess);
+            console.log(answer);
+        })
+        .catch(error => console.log(error));
 });
 
-function getWeather(city) {
+async function getWeather(city) {
     let key = '';
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-        .then(response => response.json())
-        .then(data => displayWeather(data))
-        .catch(error => console.log(error))
-}
-
-function displayWeather(data) {
-    let weatherCondition = data.weather[0].main;
-    let temperature = Math.round(parseFloat(data.main.temp) - 273.15);  // convert to celsius
-    console.log(`conditions: ${weatherCondition}, temperature: ${temperature}`);
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
 }
