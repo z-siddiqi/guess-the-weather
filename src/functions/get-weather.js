1208d628f4bc4cb8e423d92565a2c458edd65054
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-03-26 12:44:37 +0000

    refactor get-weather netlify function

--- src/functions/get-weather.js
+++ src/functions/get-weather.js
@@ -2,29 +2,30 @@
 
 const fetch = require("node-fetch");
 
-exports.handler = async function (event, context, callback) {
+exports.handler = async function (event) {
     // grab query param
     const city = event.queryStringParameters.city;
     if (!city) {
-        callback(null, {
+        return {
             statusCode: 400,
             body: "Missing 'city' parameter.",
-        });
+        }
     }
 
     // use node-fetch to call the openweathermap api
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}`);
     if (!response.ok) {
-        callback(null, {
+        return {
             statusCode: 500,
             body: "Error!",
-        });
+        }
     }
 
     // return weather data
     const json = await response.json();
-    callback(null, {
+    console.log(json);
+    return {
         statusCode: 200,
         body: JSON.stringify(json),
-    });
+    }
 };
