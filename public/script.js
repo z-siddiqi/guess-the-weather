776dc4dbd7c690298e1c2ebcad0a6c148492d3fd
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-09 13:44:16 +0100

    add simple cache

--- public/script.js
+++ public/script.js
@@ -1,27 +1,36 @@
-let form = document.getElementById("userGuessForm");
-form.addEventListener("submit", function (e) {
+const cacheTime = 150000;  // 2.5 minutes
+const cache = {};
+const form = document.getElementById("userGuessForm");
+form.addEventListener("submit", async function (e) {
     e.preventDefault();
-    let city = document.getElementById("city").value;
-    getWeather(city)
-        .then(json => {
-            let userGuess = {
-                weatherCondition: document.querySelector("input[name='condition']:checked").value,
-                temperature: +document.querySelector("input[name='temperature']").value,
-            };
-            let answer = {
-                weatherCondition: json.weather[0].main,
-                temperature: Math.round(parseFloat(json.main.temp) - 273.15),  // convert to celsius
-            };
-            let result = JSON.stringify(userGuess) === JSON.stringify(answer) ? "Correct!" : "Wrong!";
-            alert(result);
-        })
-        .catch(error => console.log(error));
+    const city = document.getElementById("city").value;
+    const weatherData = await getWeather(city, cacheTime);
+    const userGuess = {
+        weatherCondition: document.querySelector("input[name='condition']:checked").value,
+        temperature: +document.querySelector("input[name='temperature']").value,  // convert to int with +
+    };
+    const answer = {
+        weatherCondition: weatherData.weather[0].main,
+        temperature: Math.round(parseFloat(weatherData.main.temp) - 273.15),  // convert to celsius
+    };
+    const result = JSON.stringify(userGuess) === JSON.stringify(answer) ? "Correct!" : "Wrong!";
+    alert(result);
 });
 
-async function getWeather(city) {
-    let response = await fetch(`/.netlify/functions/get-weather?city=${city}`);
+async function getWeather(city, time) {
+    const now = new Date().getTime();
+    if (!cache[city] || cache[city].cacheTimer < now) {
+        cache[city] = await fetchWeather(city);
+        cache[city].cacheTimer = now + time;
+    }
+    return cache[city];
+}
+
+async function fetchWeather(city) {
+    const response = await fetch(`/.netlify/functions/fetch-weather?city=${city}`);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
-    return await response.json();
+    const weather = await response.json();
+    return weather;
 }
